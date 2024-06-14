8029d933305cf495e969a651f0dd8b3a37780654
aurelie <ferre.aurelie@wanadoo.fr>
2024-06-14 17:01:42 +0200

    Create User entity and implement Easyadmin

---
+++ veto_compar/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    protected Generator $faker;
+
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $this->faker = Factory::create('fr_FR');
+        for ($i = 0 ; $i < 5; $i++) {
+            $user = new User();
+            $user->setEmail($this->faker->email);
+            $plaintextPassword = 'password';
+            $user->setPassword($this->passwordHasher->hashPassword($user, $plaintextPassword));
+            $user->setFirstname($this->faker->firstName);
+            $user->setLastname($this->faker->lastName);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
